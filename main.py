fa28ab386c889bb726c09b812cb009693f36ece1
Aymane Hachcham <aymanehachchaming@gmail.com>
2023-11-24 18:11:42 +0100

    small modif

--- main.py
+++ main.py
@@ -2,4 +2,4 @@
 from hwe import NewsPapersExtractor
 
 if __name__ == '__main__':
-    print(NewsPapersExtractor('sn83030313').pages(limit=50, save=True))
+    NewsPapersExtractor('sn83030313').pages(limit=50, save=True)
