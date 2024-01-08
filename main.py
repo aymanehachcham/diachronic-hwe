af5739171bf8592ab94841e698f7fedeb2bf6e0e
aymanehachcham <aymanehachchaming@gmail.com>
2024-01-08 16:51:17 +0100

    updates

--- main.py
+++ main.py
@@ -2,33 +2,13 @@
 
 from hwe.data.extraction import NewsPaperExtractorXml
 from hwe.data.processor import DocumentProcessor
-import nltk
-from nltk.corpus import wordnet as wn
-
-
-def find_relations(word):
-    synsets = wn.synsets(word)
-    relations = []
-
-    for synset in synsets:
-        hypernyms = synset.hypernyms()
-        hyponyms = synset.hyponyms()
-
-    for hypernym in hypernyms:
-        relations.append((synset.name(), hypernym.name(), 'hypernym'))
-
-    for hyponym in hyponyms:
-        relations.append((synset.name(), hyponym.name(), 'hyponym'))
-
-    return relations
 
 if __name__ == '__main__':
 
-    word = 'gay'
-    relations = find_relations(word)
-
-    print(relations)
-    
+    txt = DocumentProcessor(
+        file_path='post_process_data/compiled_docs/TheNewYorkTimes1980.json'
+    ).extract_hyponyms('abuse')
+    print(txt)
 
 
        
