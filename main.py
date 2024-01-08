d487bcd4140786f43e5d2a9dac5b47f064413ee6
aymanehachcham <aymanehachchaming@gmail.com>
2024-01-08 19:02:58 +0100

    added pre-commit config

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from hwe.data.processor import DocumentProcessor
 
 if __name__ == '__main__':
 
+    # main entry point
     txt = DocumentProcessor(
         file_path='post_process_data/compiled_docs/TheNewYorkTimes1980.json'
     ).retrieve_docs('abuse')
