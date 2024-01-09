2c96d236ad20b37f0a1bb5cbd4c1778dc777365e
aymanehachcham <aymanehachchaming@gmail.com>
2024-01-09 16:33:32 +0100

    updates

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from hwe.data.processor import DocumentProcessor
 
 if __name__ == "__main__":
     # This is the main entry point
-    txt = DocumentProcessor(file_path="post_process_data/compiled_docs/TheNewYorkTimes1980.json").retrieve_docs("abuse")
-
-    print(txt)
+    docs = DocumentProcessor(json_file_path="sample_data/TheNewYorkTimes1980.xml").retrieve_context(
+        query="Financial issues with the government"
+    )
+    for doc in docs:
+        print(doc)
