1b43194edb5642b2292959723cd01d3656ccc019
aymanehachcham <aymanehachchaming@gmail.com>
2024-02-15 16:00:37 +0100

    working on the processor to imporve querying efficiency.

--- diachronic_hwe/data/processor.py
+++ diachronic_hwe/data/processor.py
@@ -5,6 +5,7 @@ import re
 from collections import Counter
 from typing import List, Optional, Union
 
+import spacy
 from nltk.corpus import stopwords, wordnet as wn
 from nltk.tokenize import RegexpTokenizer
 
@@ -27,6 +28,7 @@ class DocumentProcessor:
         if not os.path.exists(json_file_path):
             raise FileNotFoundError(f"File {json_file_path} does not exist.")
 
+        self.nlp = spacy.load("en_core_web_md")
         self.file_path = json_file_path
         self.file_name = os.path.basename(json_file_path).split(".")[0]
         self.docs = []
@@ -58,7 +60,7 @@ class DocumentProcessor:
         # Get words with frequency higher equal top_freq
         return [word for word, count in word_counts.items() if count == top_freq]
 
-    def __compile_docs(self, num_articles: int = 100, save: bool = False) -> Optional[str]:
+    def __compile_docs(self, num_articles: int = 500, save: bool = False) -> Optional[str]:
         """
         Compile all full texts into one big chunk.
         """
@@ -96,14 +98,38 @@ class DocumentProcessor:
 
         return hyponyms
 
+    def lookup_word(self, word: str) -> None:
+        """
+        Look up a word in the documents.
+        """
+        _ = self.__compile_docs()
+        logging.info(f"Length of docs: {len(self.docs)}")
+        matching_paragraphs = []
+        target_lemma = self.nlp(word)[0].lemma_.lower()
+        for doc in self.docs:
+            paragraphs = doc.split("\n")
+            # Process each paragraph with spaCy
+            for chunk in paragraphs:
+                if chunk.strip():  # Ensure paragraph is not just whitespace
+                    doc = self.nlp(chunk)
+                    # Check if any word in the paragraph has the same lemma as the target word
+                    if any(word.lemma_.lower() == target_lemma for word in doc):
+                        matching_paragraphs += [chunk]
+
+        txt = "\n".join(matching_paragraphs)
+        txt_path = str(self.file_path).rsplit("/", maxsplit=1)[-1].replace(".json", ".txt")
+        with open(os.path.join(os.getenv("COMPILED_DOCS_PATH"), txt_path), "w") as f:
+            f.write(txt)
+
     def retrieve_context(
         self,
+        target_word: str,
         query: str,
     ) -> Optional[List[str]]:
         """
         Retrieve documents that contain a given word.
         """
-        self.__compile_docs(save=True)
+        self.lookup_word(target_word)
         context = []
         rag = RAGManager.from_file(file_path=os.path.join(os.getenv("COMPILED_DOCS_PATH"), self.file_name + ".txt"))
         docs = rag.retrieve_docs(query=query)
